e25a61abd8a99b1b9ce0ece5ec55c594fb1a3ebe
Szabolcs Nógrádi <nogradiszabolcs24@gmail.com>
2025-01-13 18:08:18 +0100

    Fix footer github link

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="mt-12 pb-6">
     <p class="text-center text-sm text-slate-500">
       This website's source code is available on my
-      <NuxtLink to="https://github.com/dashboard" target="_blank" class="hover:text-gray-900 text-gray-600">GitHub</NuxtLink>
+      <NuxtLink to="https://github.com/SzabolcsN/profile-website" target="_blank" class="hover:text-gray-900 text-gray-600">GitHub</NuxtLink>
     </p>
   </footer>
 </template>
